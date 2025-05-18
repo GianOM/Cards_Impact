3ed74761a41fb09c3506f3d957065f6e2b5c69aa
GianOM <gianvitor.om@gmail.com>
2025-05-18 16:06:36 -0300

    Hide Menu

---
+++ Main_Game_Cards_Impact/Scripts/main_3d.gd
@@ -0,0 +1,19 @@
+extends Node3D
+
+@onready var main_2d: Node2D = $Main2D
+
+func hideUI():
+	main_2d.visible = false
+func showUI():
+	main_2d.visible = true
+var uiToggle: bool = false
+
+func _input(event):
+	if Input.is_action_just_pressed("hide_ui"): #just_pressed triggers only once
+		if uiToggle == false:
+			uiToggle = true
+			hideUI()
+
+		else:
+			uiToggle = false
+			showUI()
