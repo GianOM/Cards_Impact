ef7fa1fa9dbb778580771a0269fc28d7f42b408a
GianOM <gianvitor.om@gmail.com>
2025-05-18 21:16:30 -0300

    Towers with Range and Tier 2 Tower
    Adcionei as posibilidades de mudar as torres de Tier 1 para Tier 2 e coloquei o range + enemy detection

---
+++ Main_Game_Cards_Impact/Scenes/3D/Troops/troops.gd
@@ -0,0 +1,5 @@
+class_name Tropas
+extends CharacterBody3D
+
+var Vida: float = 15.0
+@export var Velocidade: float = 1.0
---
+++ Main_Game_Cards_Impact/Scripts/3D/Tower/Get_Tower_From_Database.gd
@@ -0,0 +1,22 @@
+extends MeshInstance3D
+
+const TOWERS_DATABASE = preload("res://Scenes/3D/Towers/Towers_Databese.tscn")
+@onready var collision_shape_3d: CollisionShape3D = $"../Enemy Detection Area 3D/CollisionShape3D"
+@onready var tower_range: MeshInstance3D = $"../Enemy Detection Area 3D/Tower_Range"
+
+#0.5 Radius esta para 1.0 Scale do Tower_Range Mesh Instance
+
+func set_tier_1_Mesh():
+	var database_instance = TOWERS_DATABASE.instantiate()
+	var tower_tier_1 = database_instance.get_node("Tower_Tier_1")
+	collision_shape_3d.shape.set_radius(0.5)
+	tower_range.scale = Vector3(1,1,1)
+	mesh = tower_tier_1.mesh
+
+
+func set_tier_2_Mesh():
+	var database_instance = TOWERS_DATABASE.instantiate()
+	var tower_tier_2 = database_instance.get_node("Tower_Tier_2")
+	collision_shape_3d.shape.set_radius(1)
+	tower_range.scale = Vector3(2,2,2)
+	mesh = tower_tier_2.mesh
