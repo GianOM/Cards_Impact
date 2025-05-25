f8f0a5f1f2f8f90272aee8842c7b93b8b5de0cb8
GianOM <gianvitor.om@gmail.com>
2025-05-25 18:28:01 -0300

    Fresh Commit

--- WEEK 1 BUILD/Scripts/3D/Troops/Troops_Data.gd
+++
@@ -1,9 +0,0 @@
-class_name Moving_Units_Data
-extends Resource
-
-@export var Vida: float
-@export var Velocidade: float
-@export var Dano: float
-@export var Tier: int#Variavel de 0 a 1
-
-@export_file("*.tscn") var troop_scene_path: String
