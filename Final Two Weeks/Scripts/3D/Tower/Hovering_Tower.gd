c3abc17751e8d763d7294fec7243abbfc2ec04a3
GianOM <gianvitor.om@gmail.com>
2025-05-30 03:00:18 -0300

    Hovering Towers

---
+++ Final Two Weeks/Scripts/3D/Tower/Hovering_Tower.gd
@@ -0,0 +1,36 @@
+extends Node3D
+
+
+func set_mesh_from_tier(index: int):
+	#Como fazer um Switch/Case padrao
+	match index:
+		0:
+			Hide_All_Meshes()
+			$Index_0.show()
+		1:
+			Hide_All_Meshes()
+			$Index_1.show()
+			
+		2:
+			Hide_All_Meshes()
+			$Index_2.show()
+			
+		3:
+			Hide_All_Meshes()
+			$Index_3.show()
+		4:
+			Hide_All_Meshes()
+			$Index_4.show()
+		5:
+			Hide_All_Meshes()
+			$Index_5.show()
+			
+			
+func Hide_All_Meshes():
+	$Index_0.hide()
+	$Index_1.hide()
+	$Index_2.hide()
+	$Index_3.hide()
+	$Index_4.hide()
+	$Index_5.hide()
+	
